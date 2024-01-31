40f0436e441a94264dad52afb51dd69fea99faed
gaurangdes <desai.gaurang19@gmail.com>
2024-01-31 16:43:29 +0000

    Upload New File

---
+++ src/d/Main.java.java
@@ -0,0 +1,12 @@
+public class D {
+    public static void main(String[] args) {
+        int[] dieA = {1, 2, 3, 4, 5, 6};
+        int[] dieB = {1, 2, 3, 4, 5, 6};
+
+        System.out.println("Part A:");
+        PartA.partA(dieA, dieB);
+
+        System.out.println("Part B:");
+        PartB.partB(dieA, dieB);
+    }
+}
